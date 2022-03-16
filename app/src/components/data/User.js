0782596ce9482edfd592775f16b88dce0fdc0cf7
Rayan <raeinking2@gmail.com>
2022-03-16 22:48:53 +0300

    commit

--- app/src/components/data/User.js
+++ app/src/components/data/User.js
@@ -1,46 +1,46 @@
-import React from 'react'
-import axios from 'axios'
+// import React from 'react'
+// import axios from 'axios'
 
-class User extends React.Component {
-  constructor(props){
-    super(props);
-        this.state = { users: []}
-  }
+// class User extends React.Component {
+//   constructor(props){
+//     super(props);
+//         this.state = { users: []}
+//   }
 
-    componentDidMount(){
-        //get request
-        axios.get('http://localhost/conndb.php').then(res => 
-        {
-        this.setState({users: res.data});
-        console.log(res)
-           }); 
-        }
-
-
-
-
-    render() {
-        return(
-
-            <div>
-               {this.state.users.map(result => {
-                   return(
-                       <div>
-                           <p>{result.user_Id}</p>
-                           <p>{result.user_name}</p>
-                           <p>{result.user_Pwd}</p>
-                           <p>{result.user_img}</p>
-                       </div>
-                    )
-               })}
-
-            </div>
+//     componentDidMount(){
+//         //get request
+//         axios.get('http://localhost/conndb.php').then(res => 
+//         {
+//         this.setState({users: res.data});
+//         console.log(res)
+//            }); 
+//         }
 
 
 
 
-            )
-    }
-}
+//     render() {
+//         return(
 
-export default User
+//             <div>
+//                {this.state.users.map(result => {
+//                    return(
+//                        <div>
+//                            <p>{result.user_Id}</p>
+//                            <p>{result.user_name}</p>
+//                            <p>{result.user_Pwd}</p>
+//                            <p>{result.user_img}</p>
+//                        </div>
+//                     )
+//                })}
+
+//             </div>
+
+
+
+
+//             )
+//     }
+// }
+
+// export default User
