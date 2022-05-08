9950793e9131335079691aef05d2e61e7072e666
Rayan <raeinking2@gmail.com>
2022-05-08 19:27:58 +0300

    commit

--- app/src/components/addpost/Addpost.js
+++ app/src/components/addpost/Addpost.js
@@ -2,7 +2,7 @@ import React, {useEffect , useState} from 'react'
 import styled from 'styled-components'
 import './addpost.css'
 import { getAuth , onAuthStateChanged} from "firebase/auth";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import {getDocs ,  serverTimestamp , orderBy , query , addDoc , doc, onSnapshot ,collection, getFirestore , where} from 'firebase/firestore';
 import db from '../data/firebase';
 
@@ -15,6 +15,68 @@ function Addpost() {
     const [Caption , setCaption] = useState('asdgsadg')
     const [username, setUsername] = useState('')
     const auth = getAuth();
+
+    
+
+
+
+
+
+    const storage = getStorage();
+
+    // Create the file metadata
+    /** @type {any} */
+    const metadata = {
+      contentType: 'image/jpeg'
+    };
+    
+    // Upload file and metadata to the object 'images/mountains.jpg'
+    const storageRef = ref(storage, 'images/' + file.name);
+    const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+    
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
+      }, 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     useEffect( () => {
         const po = async () =>  {
